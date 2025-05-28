962df2065e773b3ceaea50480aaa4f399f7c5210
Jonathan Muchewicz <166529327+JonathanAndresM@users.noreply.github.com>
2025-05-27 22:19:10 -0300

    Delete router directory

--- router/users.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { allUsers, usersByRole } from "../controllers/users/read.js";
-
-const router = Router()
-
-router.get("/all", allUsers)
-router.get('/role/:rol',usersByRole)
-
-export default router
